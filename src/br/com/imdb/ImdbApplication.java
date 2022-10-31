21a704c5e64be4274c96f175b03b9d8c8f82fd60
rookie-leo <proleonarpa@gmail.com>
2022-10-31 17:10:41 -0300

    [ dia 1 ] Implementada chamada ao IMDB client.

--- src/br/com/imdb/ImdbApplication.java
+++ src/br/com/imdb/ImdbApplication.java
@@ -2,17 +2,20 @@ package br.com.imdb;
 
 import br.com.imdb.gateway.client.ImdbClient;
 
+import java.net.http.HttpClient;
 import java.util.Arrays;
 
 public class ImdbApplication {
 
     public static void main(String[] args) {
-
         String apiKey = Arrays.stream(args).findFirst().get();
+        ImdbClient imdb = new ImdbClient();
 
-        ImdbClient imdb = new ImdbClient(apiKey);
+        var client = HttpClient.newHttpClient();
+        var response = imdb.findTop250Movies(apiKey, client);
 
-        System.out.println(apiKey);
+        System.out.println(response.statusCode());
+        System.out.println(response.body());
 
     }
 
--- src/br/com/imdb/gateway/client/ImdbClient.java
+++ src/br/com/imdb/gateway/client/ImdbClient.java
@@ -1,47 +1,32 @@
 package br.com.imdb.gateway.client;
 
-import java.io.FileNotFoundException;
-import java.net.Authenticator;
-import java.net.InetSocketAddress;
-import java.net.ProxySelector;
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
-import java.net.http.HttpClient.*;
 import java.net.http.HttpRequest;
-import java.net.http.HttpRequest.BodyPublishers;
-import java.nio.file.Paths;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 
 public class ImdbClient {
-
-    private String apiKey = "";//TODO - tentar receber a apiKey como argumento ao rodar o código Java
-    private String uri = "https://imdb-api.com/en/API/Top250Movies/" + apiKey + "https://imdb-api.com/en/API/Top250Movies/";
-    private HttpClient client;
     private HttpRequest request;
+    private HttpResponse<String> response;
 
-    public ImdbClient(String apiKey) {
-        this.apiKey = apiKey;
-    }
+    public HttpResponse<String> findTop250Movies(String apiKey, HttpClient client) {
+            String uri = "https://imdb-api.com/en/API/Top250Movies/" + apiKey;
 
-    public void getClient() {
-         client = HttpClient.newBuilder()
-                 .version(Version.HTTP_2)
-                 .followRedirects(Redirect.NORMAL)
-                 .proxy(ProxySelector.of(new InetSocketAddress("www-proxy.com", 8080)))
-                 .authenticator(Authenticator.getDefault())
-                 .build();
-     }
-
-    public void requestConstructor() {
-        try {
             request = HttpRequest.newBuilder()
                     .uri(URI.create(uri))
-                    .header("Content-Type", "application/json")
-                    .POST(BodyPublishers.ofFile(Paths.get("file.json")))
+                    .GET()
                     .build();
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("Arquivo não encontrado: " + e);
+
+        try {
+            response = client.send(request, BodyHandlers.ofString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
+
+        return response;
     }
-
-
 }
